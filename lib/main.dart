71bf03e6b557e890c753b869374d49dfde51b70e
Ahmad Galang <258galang@gmail.com>
2023-04-20 16:10:55 +0700

    Create class and implementation class in main function

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,35 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('This My App'),
-        ),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 56, 81, 241),
-              Color.fromARGB(109, 24, 23, 22)
-            ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello Word',
-              style: TextStyle(fontSize: 24, color: Colors.white),
-            ),
-          ),
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('My Apps'),
+      ),
+      body: GradientContainer(),
+    ),
+  )
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(19, 0, 0, 0),
+          Color.fromARGB(19, 0, 0, 0)
+        ], begin: Alignment.center, end: Alignment.center),
+      ),
+      child: const Center(
+        child: Text(
+          'Heloo World!!!!!',
+          style: TextStyle(color: Color.fromARGB(255, 255, 5, 5) , fontSize: 50),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
