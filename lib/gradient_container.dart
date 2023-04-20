fdc93799760aaa334337bee9d8156280ed5e0ba4
Ahmad Galang <258galang@gmail.com>
2023-04-20 21:01:32 +0700

    Add variable in gradient_container

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
+var alignmentCenter = Alignment.center;
+var bottomCenter = Alignment.bottomCenter;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: const [
           Color.fromARGB(255, 26, 2, 80),
           Color.fromARGB(255, 196, 0, 0)
-        ], begin: Alignment.center, end: Alignment.bottomCenter),
+        ], begin: alignmentCenter, end: bottomCenter),
       ),
       child: const Center(
         child: Text(
-          'Heloo World!!!!!',
+          'Helloo World!!!!!',
           style: TextStyle(color: Color.fromARGB(255, 255, 5, 5), fontSize: 50),
         ),
       ),
